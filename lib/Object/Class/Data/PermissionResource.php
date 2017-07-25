816f4fe7763b1e073b84ad67f6c2cd6d0a91651a
Christian Fasching <christian.fasching@elements.at>
2017-07-25 11:02:34 +0200

    added license

--- lib/Object/Class/Data/PermissionResource.php
+++ lib/Object/Class/Data/PermissionResource.php
@@ -1,19 +1,17 @@
-<?php 
+<?php
 /**
  * Pimcore
  *
- * LICENSE
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
  *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://www.pimcore.org/license
- *
- * @category   Pimcore
- * @package    Object_Class
- * @copyright  Copyright (c) 2009-2013 pimcore GmbH (http://www.pimcore.org)
- * @license    http://www.pimcore.org/license     New BSD License
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
  */
+
 
 class Object_Class_Data_PermissionResource extends Object_Class_Data_Select {
 
