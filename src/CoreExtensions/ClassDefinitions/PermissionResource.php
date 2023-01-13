094f219a91b06a376661b276d31dea8edc1b49b1
Christian Fasching <christian.fasching@pimcore.com>
2023-01-13 11:49:33 +0100

    return type fixes

--- src/CoreExtensions/ClassDefinitions/PermissionResource.php
+++ src/CoreExtensions/ClassDefinitions/PermissionResource.php
@@ -63,7 +63,7 @@ class PermissionResource extends Select
         $this->configureOptions();
     }
 
-    public static function __set_state(array $data): static
+    public static function __set_state(/* array */ $data): static
     {
         $obj = parent::__set_state($data);
         $obj->configureOptions();
