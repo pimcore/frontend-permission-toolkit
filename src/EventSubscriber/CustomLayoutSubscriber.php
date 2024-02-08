45ef1ff2fca950f3a03c76894317ed5cedda4bd3
markus-moser <markus-moser@users.noreply.github.com>
2024-02-08 08:46:18 +0000

    Apply php-cs-fixer changes

--- src/EventSubscriber/CustomLayoutSubscriber.php
+++ src/EventSubscriber/CustomLayoutSubscriber.php
@@ -1,6 +1,19 @@
 <?php
 declare(strict_types=1);
 
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
 namespace FrontendPermissionToolkitBundle\EventSubscriber;
 
 use FrontendPermissionToolkitBundle\CoreExtensions\ClassDefinitions\DynamicPermissionResource;
@@ -28,9 +41,9 @@ final class CustomLayoutSubscriber implements EventSubscriberInterface
         $this->resetPermissionResources($customLayout->getLayoutDefinitions());
     }
 
-    private function resetPermissionResources(ClassDefinition\Data|ClassDefinition\Layout|null $layout): void
+    private function resetPermissionResources(ClassDefinition\Data | ClassDefinition\Layout | null $layout): void
     {
-        if($layout === null) {
+        if ($layout === null) {
             return;
         }
 
