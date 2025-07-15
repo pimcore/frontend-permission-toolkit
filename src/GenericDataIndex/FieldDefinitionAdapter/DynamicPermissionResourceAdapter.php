ec8226739f1021f4c078a8a8707da39bfed15320
Marco Perberschlager <marco.perberschlager@pimcore.com>
2025-07-15 16:06:56 +0200

    Updated adapter to reuse existing code

--- src/GenericDataIndex/FieldDefinitionAdapter/DynamicPermissionResourceAdapter.php
+++ src/GenericDataIndex/FieldDefinitionAdapter/DynamicPermissionResourceAdapter.php
@@ -13,8 +13,12 @@ declare(strict_types=1);
 
 namespace FrontendPermissionToolkitBundle\GenericDataIndex\FieldDefinitionAdapter;
 
+use FrontendPermissionToolkitBundle\CoreExtensions\ClassDefinitions\DynamicPermissionResource;
 use Pimcore\Bundle\GenericDataIndexBundle\Enum\SearchIndex\DefaultSearch\AttributeType;
+use Pimcore\Bundle\GenericDataIndexBundle\SearchIndexAdapter\DataObject\FieldDefinitionServiceInterface;
 use Pimcore\Bundle\GenericDataIndexBundle\SearchIndexAdapter\DefaultSearch\DataObject\FieldDefinitionAdapter\AbstractAdapter;
+use Pimcore\Bundle\GenericDataIndexBundle\SearchIndexAdapter\IndexMappingServiceInterface;
+use Pimcore\Bundle\GenericDataIndexBundle\Service\SearchIndex\SearchIndexConfigServiceInterface;
 use Pimcore\Normalizer\NormalizerInterface;
 
 /**
@@ -22,17 +26,36 @@ use Pimcore\Normalizer\NormalizerInterface;
  */
 final class DynamicPermissionResourceAdapter extends AbstractAdapter
 {
+    public function __construct(
+        protected SearchIndexConfigServiceInterface $searchIndexConfigService,
+        protected FieldDefinitionServiceInterface $fieldDefinitionService,
+        private readonly IndexMappingServiceInterface $indexMappingService,
+    ) {
+        parent::__construct(
+            $searchIndexConfigService,
+            $fieldDefinitionService
+        );
+    }
+
     public function getIndexMapping(): array
     {
-        return [
-            'properties' => [
-                'key' => [
-                    'type' => AttributeType::TEXT,
-                ],
-                'value' => [
-                    'type' => AttributeType::TEXT,
-                ],
-            ],
-        ];
+        return $this->indexMappingService->getMappingForTextKeyword(
+            $this->searchIndexConfigService->getSearchAnalyzerAttributes()
+        );
+    }
+
+    public function normalize(mixed $value): ?array
+    {
+        $fieldDefinition = $this->getFieldDefinition();
+        if (!$fieldDefinition instanceof DynamicPermissionResource) {
+            return null;
+        }
+
+        $normalizedValues = [];
+        foreach($value as $item) {
+            $normalizedValues[] = $item;
+        }
+
+        return $normalizedValues;
     }
 }
