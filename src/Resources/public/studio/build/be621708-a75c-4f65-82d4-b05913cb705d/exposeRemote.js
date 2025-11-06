888f348e25da3407838871c55d614e2d139093e9
markus-moser <4639428+markus-moser@users.noreply.github.com>
2025-11-06 17:30:36 +0000

    Automatic frontend build

---
+++ src/Resources/public/studio/build/be621708-a75c-4f65-82d4-b05913cb705d/exposeRemote.js
@@ -0,0 +1,7 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      window.pluginRemotes.pimcore_frontendpermissiontoolkit_bundle = "/bundles/frontendpermissiontoolkit/studio/build/be621708-a75c-4f65-82d4-b05913cb705d/static/js/remoteEntry.js"
+    
