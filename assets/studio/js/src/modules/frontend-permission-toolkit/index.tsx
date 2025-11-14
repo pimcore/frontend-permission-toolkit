0912143c947eba64d4186c0e67193c889c25f7f0
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2025-11-14 11:58:27 +0100

    Add 'header' plugin to ESLint configuration

--- assets/studio/js/src/modules/frontend-permission-toolkit/index.tsx
+++ assets/studio/js/src/modules/frontend-permission-toolkit/index.tsx
@@ -1,3 +1,13 @@
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
 import { type AbstractModule, container } from '@pimcore/studio-ui-bundle'
 import { serviceIds } from '@pimcore/studio-ui-bundle/app'
 import { type DynamicTypeObjectDataRegistry } from '@pimcore/studio-ui-bundle/modules/element'
