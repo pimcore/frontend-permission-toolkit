e0cd9e8109dc52d5b8e4cb321b6cd1658540fb69
ValeriaMaltseva <11871778+ValeriaMaltseva@users.noreply.github.com>
2025-11-14 11:02:30 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/845089f5-3297-49d0-926d-25f7f01b6211/exposeRemote.js
+++
@@ -1,7 +0,0 @@
-
-      if (window.pluginRemotes === undefined) {
-        window.pluginRemotes = {}
-      }
-
-      window.pluginRemotes.pimcore_frontendpermissiontoolkit_bundle = "/bundles/frontendpermissiontoolkit/studio/build/845089f5-3297-49d0-926d-25f7f01b6211/static/js/remoteEntry.js"
-    
