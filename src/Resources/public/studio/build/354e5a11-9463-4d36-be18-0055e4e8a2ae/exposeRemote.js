51bbb15e125f2fc520feabdd048d7887fb238c9e
robertSt7 <104770750+robertSt7@users.noreply.github.com>
2026-03-26 08:31:49 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/354e5a11-9463-4d36-be18-0055e4e8a2ae/exposeRemote.js
+++ src/Resources/public/studio/build/354e5a11-9463-4d36-be18-0055e4e8a2ae/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_frontendpermissiontoolkit_bundle = "/bundles/frontendpermissiontoolkit/studio/build/373a789b-6ea7-482b-b85d-9d0108be9959/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_frontendpermissiontoolkit_bundle = "/bundles/frontendpermissiontoolkit/studio/build/354e5a11-9463-4d36-be18-0055e4e8a2ae/static/js/remoteEntry.js"
 
       
     
