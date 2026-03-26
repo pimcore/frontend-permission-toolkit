37bcb02c65a9ba9e1fcaba688f57265c6be40862
fashxp <8792145+fashxp@users.noreply.github.com>
2026-03-26 11:20:02 +0000

    Automatic frontend build

---
+++ src/Resources/public/studio/build/bc1c611c-22f9-416e-9971-30c22a193015/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.pimcore_frontendpermissiontoolkit_bundle = "/bundles/frontendpermissiontoolkit/studio/build/bc1c611c-22f9-416e-9971-30c22a193015/static/js/remoteEntry.js"
+
+      
+    
