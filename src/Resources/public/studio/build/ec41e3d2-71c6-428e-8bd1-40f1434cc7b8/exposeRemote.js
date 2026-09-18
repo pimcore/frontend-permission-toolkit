082b6e703171a493427bc16d108c1119de8ac6e4
ValeriaMaltseva <11871778+ValeriaMaltseva@users.noreply.github.com>
2026-09-18 13:21:54 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/ec41e3d2-71c6-428e-8bd1-40f1434cc7b8/exposeRemote.js
+++ src/Resources/public/studio/build/ec41e3d2-71c6-428e-8bd1-40f1434cc7b8/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_frontendpermissiontoolkit_bundle = "/bundles/frontendpermissiontoolkit/studio/build/1bc2e51f-87e3-4fa1-b567-8b62ab21100d/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_frontendpermissiontoolkit_bundle = "/bundles/frontendpermissiontoolkit/studio/build/ec41e3d2-71c6-428e-8bd1-40f1434cc7b8/static/js/remoteEntry.js"
 
       
     
